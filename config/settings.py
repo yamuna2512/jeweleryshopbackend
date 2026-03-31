53909a2891f58a4787c1be01bd574ccf1325b6ec
yamuna <yamuna.pdy@gmail.com>
2026-03-31 10:18:08 +0300

    updated frontend

--- config/settings.py
+++ config/settings.py
@@ -57,12 +57,12 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS =  [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://jeweleryshopsite.netlify.app",
+    "https://jeweleryshopfrontend.netlify.app",
     ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://jeweleryshopsite.netlify.app",
+    "https://jeweleryshopfrontend.netlify.app",
     ]
 
 
